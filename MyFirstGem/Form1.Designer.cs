d43c7bf6b1ec0f68f7168561390b2123c29c103f
Ailouan <ailouane.elmakoudi@gmail.com>
2022-03-19 14:27:53 +0000

    My first commit

---
+++ MyFirstGem/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MyFirstGem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            this.enemyTow = new System.Windows.Forms.PictureBox();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.BackColor = System.Drawing.Color.Transparent;
+            this.enemyOne.Image = ((System.Drawing.Image)(resources.GetObject("enemyOne.Image")));
+            this.enemyOne.Location = new System.Drawing.Point(37, 35);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(100, 85);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // enemyTow
+            // 
+            this.enemyTow.BackColor = System.Drawing.Color.Transparent;
+            this.enemyTow.Image = ((System.Drawing.Image)(resources.GetObject("enemyTow.Image")));
+            this.enemyTow.Location = new System.Drawing.Point(266, 35);
+            this.enemyTow.Name = "enemyTow";
+            this.enemyTow.Size = new System.Drawing.Size(100, 85);
+            this.enemyTow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyTow.TabIndex = 0;
+            this.enemyTow.TabStop = false;
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.BackColor = System.Drawing.Color.Transparent;
+            this.enemyThree.Image = ((System.Drawing.Image)(resources.GetObject("enemyThree.Image")));
+            this.enemyThree.Location = new System.Drawing.Point(490, 35);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(100, 85);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyThree.TabIndex = 0;
+            this.enemyThree.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bullet.BackColor = System.Drawing.Color.White;
+            this.bullet.Image = ((System.Drawing.Image)(resources.GetObject("bullet.Image")));
+            this.bullet.Location = new System.Drawing.Point(308, 458);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 0;
+            this.bullet.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(256, 491);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(110, 98);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtScore.Location = new System.Drawing.Point(12, 200);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(610, 175);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTemerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(634, 601);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.enemyThree);
+            this.Controls.Add(this.enemyTow);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.txtScore);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fighter jet shooter game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisUp);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox enemyTow;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ MyFirstGem/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstGem
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, shooting, isGameOver;
+        int score;
+        int playerSpeed = 12;
+        int enemySpeed;
+        int bulletSpeed;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            reseGame();
+        }
+
+        private void mainGameTemerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = score.ToString();
+            enemyOne.Top += enemySpeed;
+            enemyTow.Top += enemySpeed;
+            enemyThree.Top += enemySpeed;
+
+            // Player movement logic starts
+            if(enemyOne.Top > 655 || enemyTow.Top > 655 || enemyThree.Top > 655)
+            {
+                gameOver();
+            }
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+            if(goRight == true && player.Left < 510)
+            {
+                player.Left += playerSpeed;
+            }
+            // Player movement logic ends
+            if(shooting== true)
+            {
+                bulletSpeed = 15;
+                bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                bullet.Left = -300;
+                bulletSpeed = 0;
+            }
+            if(bullet.Top < -20)
+            {
+                shooting = false;
+            }
+        }
+
+        private void keyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode== Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if(e.KeyCode == Keys.Space || shooting == false)
+            {
+                shooting = true;
+                bullet.Top = player.Top - 30;
+                bullet.Left = player.Left + (player.Left /4);
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                reseGame(); 
+            }
+        }
+        private void reseGame()
+        {
+            gameTimer.Start();
+            enemySpeed = 4;
+
+            enemyOne.Left = rnd.Next(20, 525);
+            enemyTow.Left = rnd.Next(20, 525);
+            enemyThree.Left = rnd.Next(20, 525);
+
+            enemyOne.Top = rnd.Next(0, 200) * -1;
+            enemyTow.Top = rnd.Next(0, 500) * -1;
+            enemyThree.Top = rnd.Next(0, 900) * -1;
+
+            score = 0;
+            bulletSpeed = 0;
+            bullet.Left = -300;
+            shooting = false;
+
+            txtScore.Text = score.ToString();
+        }
+        private void gameOver()
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text += Environment.NewLine + "Game Over!!" + Environment.NewLine + "Press Enter to try again.";
+
+        }
+    }
+}
